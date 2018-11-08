671d0be045b9c46816bdb4c75a0ca74af20fa98c
samvancart <sam02cart@gmail.com>
2018-11-08 19:39:45 +0200

    viikko2 osa2 lopullinen

--- laskarit/viikko2/Unicafe/src/main/java/com/mycompany/unicafe/Paaohjelma.java
+++ laskarit/viikko2/Unicafe/src/main/java/com/mycompany/unicafe/Paaohjelma.java
@@ -5,6 +5,7 @@ public class Paaohjelma {
     public static void main(String[] args) {
         Kassapaate unicafeExactum = new Kassapaate();
         Maksukortti kortti = new Maksukortti(10000);
+
         
         unicafeExactum.syoEdullisesti(kortti);
         
